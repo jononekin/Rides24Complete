5871a7aee1f436bab4f923c8274488f03c4bfae5
MEtxebe <124911473+MEtxebe@users.noreply.github.com>
2024-10-06 22:43:09 +0200

    testsGtBooking

---
+++ src/test/java/test/GetBookingFromDriverDBBlackTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import dataAccess.DataAccess;
+import domain.Booking;
+import domain.Driver;
+
+public class GetBookingFromDriverDBBlackTest {
+
+}
